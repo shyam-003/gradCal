ad42d793e9a4e506cadc16ea572d848a2fc7bfdb
Shyam <shyam.03@icloud.com>
2024-06-21 20:43:08 +0530

    updated home screen

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -11,13 +11,6 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -104,20 +97,9 @@ class _MyHomePageState extends State<MyHomePage> {
               },
               child: Text('File Picker Screen'),
             ),
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
+            
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
       ),
     );
   }
